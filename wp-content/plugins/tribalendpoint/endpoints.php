d968716d55c5818fda83f870203ed3ddcacb577f
Herald-Flores-Rizo <herald.flores@boombit.agency>
2021-02-05 08:21:49 -0600

    fix endpoint

--- wp-content/plugins/tribalendpoint/endpoints.php
+++ wp-content/plugins/tribalendpoint/endpoints.php
@@ -2,10 +2,6 @@
 add_action('rest_api_init', 'wp_rest_cupones_endpoints');
 
 function wp_rest_cupones_endpoints($request) {
-    /**
-     * Handle Register User request.
-     */
-
     register_rest_route('wp/v2', 'get/resources', array(
         'methods' => 'GET',
         'callback' => 'wc_show_resources_handler',
@@ -13,13 +9,28 @@ function wp_rest_cupones_endpoints($request) {
     ));
 }
 
-//Cupones canjeados by user id
 function wc_show_resources_handler($request=null){
     $params = $request->get_params();
+    $searchTerm = $params['term'];
+
+    if(count(get_itunes($searchTerm)) > 0){
+        $response = get_itunes($searchTerm);
+        array_push($response, "Itunes");
+        echo $response;
+    }elseif(count(get_tvshow($searchTerm)) > 0){
+        $response = get_tvshow($searchTerm);
+        array_push($response, "TVmaze");
+        echo $response;
+    }
+}
+
+
+function get_itunes($searchTerm){
     $curlItunes = curl_init();
+    $urlItunes = "https://itunes.apple.com/search?term=".$searchTerm;
 
     curl_setopt_array($curlItunes, array(
-        CURLOPT_URL => "https://itunes.apple.com/search?term=jack+johnson",
+        CURLOPT_URL => $urlItunes,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => "",
         CURLOPT_MAXREDIRS => 10,
@@ -32,10 +43,13 @@ function wc_show_resources_handler($request=null){
     $responseItunes = curl_exec($curlItunes);
     echo $responseItunes;
     curl_close($curlItunes);
+}
 
+function get_tvshow($searchTerm){
+    $urlTVmaze = "http://api.tvmaze.com/search/shows?q=". $searchTerm;
     $curltvmaze = curl_init();
     curl_setopt_array($curltvmaze, array(
-        CURLOPT_URL => "http://api.tvmaze.com/singlesearch/shows?q=girls",
+        CURLOPT_URL => $urlTVmaze,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => "",
         CURLOPT_MAXREDIRS => 10,
@@ -48,9 +62,6 @@ function wc_show_resources_handler($request=null){
     $responsetvmaze = curl_exec($curltvmaze);
     
     curl_close($curltvmaze);
-    
-    $response = array_merge($responseItunes,$responsetvmaze);
 
-    echo $response;
-
+    echo $responsetvmaze;
 }
---
+++ wp-content/plugins/tribalendpoint/readme.md
@@ -0,0 +1,4 @@
+##The API is accesible for the url: http://localhost/tribalMnc/wp-json/wp/v2/get/resources?term=supernatural
+
+
+The parameter term is required
